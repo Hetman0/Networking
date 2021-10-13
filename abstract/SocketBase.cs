92b75db6f501e8f474efef44933784240ab77d00
Hetman0 <gywfui@yahoo.com>
2021-10-13 14:26:59 +0100

    udp client finished

--- abstract/SocketBase.cs
+++ abstract/SocketBase.cs
@@ -4,7 +4,7 @@ using System.Net.Sockets;
 
 namespace Networking
 {
-    public abstract class SocketBase : IDisposable
+    public abstract partial class SocketBase : IDisposable
     {
         Socket m_socket;
         AsyncProvider<byte[]> m_sendBuffer, m_receiveBuffer;
@@ -28,57 +28,5 @@ namespace Networking
 
         protected abstract void Dispose();
         void IDisposable.Dispose() { Dispose(); }
-
-        public abstract class UDP : SocketBase
-        {
-            
-
-            public UDP(byte ip0, byte ip1, byte ip2, byte ip3, ushort port, int receiveBuffer, int sendBuffer)
-                : base(ip0, ip1, ip2, ip3, port, SocketType.Dgram, ProtocolType.Udp, receiveBuffer, sendBuffer) 
-            {
-                byte[] buffer = m_receiveBuffer.Pop();
-                SocketError err;
-                m_socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, out err, OnReceiveCallback, buffer);
-            }
-            protected override void Dispose() { m_socket.Close(0); }
-
-            public void Send(EndPoint target, byte[] data, int offset, int size)
-            {
-                byte[] buffer = m_sendBuffer.Pop();
-                Array.Copy(data, offset, buffer, 0, size);
-                m_socket.BeginSendTo(buffer, 0, size, SocketFlags.None, target, OnSendCallback, buffer);
-            }
-            /// <summary>
-            /// Is called when receiving data
-            /// </summary>
-            /// <param name="data">Received data, WARNING: don't resize</param>
-            /// <param name="size">Amount of received data, any additional bytes are random trush and should be ignored</param>
-            protected abstract void OnReceiveAsync(byte[] data, int size);
-
-            protected virtual void OnReceiveCallback(IAsyncResult result)
-            {
-                SocketError errEnd;
-                int size = m_socket.EndReceive(result, out errEnd);
-
-                byte[] buffer = m_receiveBuffer.Pop();
-                SocketError errBeg;
-                m_socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, out errBeg, OnReceiveCallback, buffer);
-
-                buffer = result.AsyncState as byte[];
-                int bufferSize = buffer.Length;
-                OnReceiveAsync(buffer, size);
-                if(bufferSize != buffer.Length)
-                {
-                    throw new Exception("Buffer Lenght changed");
-                }
-                m_receiveBuffer.Push(buffer);
-            }
-            protected virtual void OnSendCallback(IAsyncResult result)
-            {
-                SocketError err;
-                m_socket.EndSend(result, out err);
-                m_sendBuffer.Push(result.AsyncState as byte[]);
-            }
-        }
     }
 }
--- static/ExtensionMethod.cs
+++ static/ExtensionMethod.cs
@@ -6,5 +6,34 @@ namespace Networking
 {
     public static class ExtensionMethod
     {
+        public static string EnumerateToString<T>(this IEnumerable<T> enumerable, string separator = null, int separatorRepetitiveness = 0)
+        {
+            return EnumerateToString<T>(enumerable.GetEnumerator(), separator, separatorRepetitiveness);
+        }
+        public static string EnumerateToString<T>(this IEnumerator<T> enumerator, string separator = null, int separatorRepetitiveness = 0)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (enumerator.MoveNext())
+            {
+                builder.Append(enumerator.Current);
+                while (enumerator.MoveNext())
+                {
+                    int i = 0;
+                    if (i == separatorRepetitiveness)
+                    {
+                        if (separator != null)
+                        {
+                            builder.Append(separator);
+                        }
+                        i = 0;
+                    }
+                    else
+                    { i++; }
+
+                    builder.Append(enumerator.Current);
+                }
+            }
+            return builder.ToString();
+        }
     }
 }
